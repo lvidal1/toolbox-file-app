2430059596b481e474db59a7a855fc34c2aa7c74
lvidal1 <lvidal910@gmail.com>
2023-05-10 23:34:25 -0500

    Get all files in json format

--- backend/src/domain/usecases/GetFiles.js
+++ backend/src/domain/usecases/GetFiles.js
@@ -1,12 +1,20 @@
 import FileRepository from '../repositories/FileRepository'
 import FileService from '../services/FileService'
+import GetFile from './GetFile'
 
 class GetFiles {
   static async execute () {
     const repository = new FileRepository(new FileService())
 
-    const files = await repository.getFiles()
-    return files
+    const { files } = await repository.getFiles()
+
+    const response = []
+
+    for (let i = 0; i < files.length; i++) {
+      response.push(await GetFile.execute(files[i]))
+    }
+
+    return response
   }
 }
 
