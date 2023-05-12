d5f5242e9fbd841d4321ec2f7b28488a2c4babf1
lvidal1 <lvidal910@gmail.com>
2023-05-11 22:48:21 -0500

    Add initial files

---
+++ frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build')
+    },
+    port: 3000
+  },
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader',
+        options: { limit: false }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+}
