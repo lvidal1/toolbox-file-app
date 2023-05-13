e544d0db7d8c869b1a3b3dfb990a25ee5f6edef1
lvidal1 <lvidal910@gmail.com>
2023-05-12 22:28:36 -0500

    Add dotenv

--- frontend/src/store/slices/fileSlice.js
+++ frontend/src/store/slices/fileSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl = 'http://localhost:5000/'
+const baseUrl = process.env.API_URL
 
 const fileSlice = createApi({
   reducerPath: 'fileApi',
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   devServer: {
@@ -38,7 +39,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html')
-    })
+    }),
+    new Dotenv()
   ],
   resolve: {
     extensions: ['*', '.js', '.jsx']
