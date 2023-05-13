1430a5e00dbac1d316067e18cb3ab7008b8c55ad
lvidal1 <lvidal910@gmail.com>
2023-05-13 14:57:07 -0500

    Add fontawesome icons & apply them on components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import GithubIcon from './GithubIcon';
+import PortfolioIcon from './PortfolioIcon';
 
 export default function Footer() {
   return (
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
-        <p className="col-md-4 mb-0 text-muted">© 2023 Toolbox File App</p>
+        <p className="col-md-4 mb-0 text-muted d-flex">© 2023 Toolbox File App</p>
         <ul className="nav col-md-4 justify-content-end">
           <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-muted">
-              Home
+            <a
+              href="https://github.com/lvidal1/toolbox-file-app"
+              target="_blank"
+              className="nav-link px-2 text-muted"
+              rel="noreferrer"
+              title="Github repo">
+              <GithubIcon />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://lvidal.pe/"
+              target="_blank"
+              className="nav-link px-2 text-muted"
+              rel="noreferrer"
+              title="Portfolio">
+              <PortfolioIcon />
             </a>
           </li>
         </ul>
---
+++ frontend/src/components/PortfolioIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJs } from '@fortawesome/free-brands-svg-icons';
+
+const PortfolioIcon = () => {
+  return <FontAwesomeIcon icon={faJs} size="xl" />;
+};
+
+export default PortfolioIcon;
